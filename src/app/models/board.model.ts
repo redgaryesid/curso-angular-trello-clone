32a72654510f8c9591031e268655c91f29a1ef64
redgaryesid <r.edgaryesid@gmail.com>
2025-03-03 22:13:34 -0500

    Clase 5, Typing colors manejo de colores por medio de clases y uso de modelos para el manejo de las clases variables.

--- src/app/models/board.model.ts
+++ src/app/models/board.model.ts
@@ -1,8 +1,9 @@
+import { Colors } from "./colors. model";
 import { User } from "./user.model";
 
 export interface Board {
   id: string;
   title: string;
-  backgroundColor: 'sky'|'yellow'|'red'|'violet'|'gray'|'green';
+  backgroundColor: Colors;
   members: User[];
 }
--- src/app/modules/shared/components/card-color/card-color.component.ts
+++ src/app/modules/shared/components/card-color/card-color.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Colors,COLORS } from '@models/colors. model';
 
 
 @Component({
@@ -7,40 +8,10 @@ import { Component, Input } from '@angular/core';
 })
 export class CardColorComponent {
 
-  @Input() color: 'sky'|'yellow'|'red'|'violet'|'gray'|'green' = 'sky';
+  @Input() color: Colors = 'sky';
 
-  mapColors = {
-    sky:{
-      'bg-sky-700': true,
-      'hover:bg-sky-800': true,
-      'text-white': true
-    },
-    yellow:{
-      'bg-yellow-700': true,
-      'hover:bg-yellow-800': true,
-      'text-white': true
-    },
-    red:{
-      'bg-red-700': true,
-      'hover:bg-red-800': true,
-      'text-white': true
-    },
-    violet:{ 
-      'bg-violet-700': true,
-      'hover:bg-violet-800': true,
-      'text-white': true
-    },
-    gray:{
-      'bg-gray-700': true,
-      'hover:bg-gray-800': true,
-      'text-white': true  
-    },
-    green:{
-      'bg-green-700': true,
-      'hover:bg-green-800': true,
-      'text-white': true
-    }
-  }
+  mapColors = COLORS;
+
   get colors(){
     const clases = this.mapColors[this.color];
     return clases? clases:{};
