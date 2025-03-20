adfeed843f7f9943b23d7950e3fc7745efbc0170
redgaryesid <r.edgaryesid@gmail.com>
2025-03-19 21:29:28 -0500

    Clase 13, Crear un nuevo board: Conectar con la API.

--- src/app/modules/layout/components/board-form/board-form.component.ts
+++ src/app/modules/layout/components/board-form/board-form.component.ts
@@ -1,26 +1,43 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Router } from '@angular/router';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+
+
+import { BoardsService } from '@services/boards.service';
+import { Colors } from '@models/colors. model';
 @Component({
   selector: 'app-board-form',
   templateUrl: './board-form.component.html'
 })
 export class BoardFormComponent {
   
-  form = this.formBuilder.group({
-    title: [''],
-    backgroundColor: ['']
+  form = this.formBuilder.nonNullable.group({
+    title: ['',[Validators.required]],
+    //Al hacerlo de esta manera, evitamos que se pueda ingresar cualquier valor, 
+    //Ahora solo va a recibir valores de tipo Colors.
+    backgroundColor: new FormControl<Colors>('sky', {
+      nonNullable: true,
+      validators: [Validators.required]
+    })
   });
 
   constructor(
-    private readonly formBuilder: FormBuilder
+    private readonly formBuilder: FormBuilder,
+    private readonly boardsService: BoardsService,
+    private readonly router: Router
   ){
 
   }
 
   doSave(){
     if(this.form.valid){
-      const {title, backgroundColor} = this.form.value; 
+      const {title, backgroundColor} = this.form.getRawValue(); 
       console.log(title, backgroundColor);
+      this.boardsService.createBoard(title, backgroundColor)
+      .subscribe((board) => {
+        console.log(board);
+        this.router.navigate(['/app/boards', board.id]);
+      });
     }else{
       this.form.markAllAsTouched();
     }
--- src/app/services/boards.service.ts
+++ src/app/services/boards.service.ts
@@ -5,6 +5,7 @@ import { environment } from '@environments/environment';
 import { checkToken } from '@interceptors/token.interceptor';
 import { Board } from '@models/board.model';
 import { Card } from '@models/card.model';
+import { Colors } from '@models/colors. model';
 
 @Injectable({
   providedIn: 'root',
@@ -19,6 +20,15 @@ export class BoardsService {
     return this.http.get<Board>(`${this.apiUrl}/api/v1/boards/${id}`, {
       context: checkToken(),
     });
+  }
+
+  createBoard(title:Board['title'], backgroundColor:Colors) {
+
+    return this.http.post<Board>(`${this.apiUrl}/api/v1/boards`,{ title, backgroundColor },
+      {
+        context: checkToken(),
+      }
+    );
   }
 
   getPositions(cards:Card[],currentIndex:number) {
