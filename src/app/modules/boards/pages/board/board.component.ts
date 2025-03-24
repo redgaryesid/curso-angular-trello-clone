4a4fb00cf45ab5d91815222030304a5fb44de9ea
redgaryesid <r.edgaryesid@gmail.com>
2025-03-23 23:46:15 -0500

    Clase 15, Crear una nueva card:UI, creación de tareas nuevas sin formulario, solo funcionalidad basica

--- src/app/modules/boards/pages/board/board.component.ts
+++ src/app/modules/boards/pages/board/board.component.ts
@@ -12,6 +12,7 @@ import { BoardsService } from '@services/boards.service';
 import { CardsService } from '@services/cards.service';
 import { Board } from '@models/board.model';
 import { Card } from '@models/card.model';
+import { List } from '@models/list.model';
 
 @Component({
   selector: 'app-board',
@@ -104,5 +105,22 @@ export class BoardComponent {
     this.cardsService.updated(card.id,{position,listId}).subscribe((cardUpdated) => {
       console.log(cardUpdated);
     });
+  }
+
+  openFormCard(list:List){
+    if(this.board?.lists){
+      this.board.lists = this.board.lists.map(iteratorList => {
+        if(iteratorList.id === list.id){
+          return {
+            ...iteratorList,
+            showCardForm: true
+          }
+        }
+        return {
+          ...iteratorList,
+          showCardForm: false
+        }
+      });
+    }
   }
 }
